da9460cf4c3ffd4e4fe05159a3dc43f2e870e320
sazad44 <530@holbertonschool.com>
2018-11-12 19:12:01 +0000

    Uploading header file

---
+++ simple_shell.h
@@ -0,0 +1,14 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+char **_strtok(char *str);
+
+void proc(char *input);
+#endif
