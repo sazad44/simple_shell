4522adf05927e6a01aa525700e2ddc30ebe19c3b
wescottsharples <wesc@ttsharpl.es>
2018-11-14 21:07:30 +0000

    Remove redundant helper functions

--- token_helpers.c
+++ token_helpers.c
@@ -1,40 +1,6 @@
 #include "simple_shell.h"
 
 /**
- * _strlen - Returns the number of characters in a string.
- * @str: the string to find the length of
- *
- * Return: Length of the string.
- */
-unsigned int _strlen(char *str)
-{
-	unsigned int len = 0;
-
-	while (str[len])
-		len++;
-
-	return (len);
-}
-
-/**
- * _strcpy - copies string from one pointer to another
- * @src: the pointer to copy from
- * @dest: the pointer to copy to
- * Return: the char pointer of the destination pointer
- */
-char *_strcpy(char *src, char *dest)
-{
-	int i;
-
-	if (src == NULL || dest == NULL)
-		return (NULL);
-	for (i = 0; src[i]; i++)
-		dest[i] = src[i];
-	dest[i] = src[i];
-	return (dest);
-}
-
-/**
  * count_tokens - counts the number of tokens to be copied
  * @input: the input string to analyze for tokens
  * @delim: the delimeter string to base tokenization on
