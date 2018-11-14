7f84468f68e8e6202400d63b529b430b2483a32f
wescottsharples <wesc@ttsharpl.es>
2018-11-14 21:12:14 +0000

    Add _getenv() function that finds specified env var

--- exe_helpers.c
+++ exe_helpers.c
@@ -15,3 +15,37 @@ void _free(unsigned int num, ...)
 		free(va_arg(v_ls, char *));
 	va_end(v_ls);
 }
+
+/**
+ * _getenv - gets the specified environment variable
+ * @env: the user's environment
+ * @name: the name of the environment variable to be got
+ * Return: Pointer to the matching environment variable,
+ * otherwise NULL.
+ */
+char *_getenv(char **env, const char *name)
+{
+	extern char **environ;
+	const char *namecpy;
+	int i, j;
+
+	for (i = 0; environ[i]; i++)
+	{
+		for (j = 0; environ[i][j]; j++)
+		{
+			namecpy = name;
+			while (environ[i][j] == *namecpy)
+			{
+				j++;
+				namecpy++;
+			}
+			if (*namecpy == '\0')
+			{
+				environ[i] += j + 1;
+				return (environ[i]);
+			}
+		}
+	}
+
+	return (NULL);
+}
