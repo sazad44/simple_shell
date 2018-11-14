95c52c729288e2c05f57159022b63fe1bf77956d
sazad44 <530@holbertonschool.com>
2018-11-14 22:53:03 +0000

    Update header file

--- simple_shell.h
+++ simple_shell.h
@@ -28,7 +28,7 @@ int _strcmp(char *s1, char *s2);
 
 char *_strcat(char *dest, const char *src, const char *delim);
 
-char *_getenv(const char *name);
+char *_getenv(char **env, const char *name);
 
 int proc(char *input, char *ipname, char **env);
 
