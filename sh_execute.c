baee238451154821d8a6925c572ba07ada2f168f
sazad44 <530@holbertonschool.com>
2018-11-20 00:15:39 +0000

    Update proc function to appease betty

--- sh_execute.c
+++ sh_execute.c
@@ -1,22 +1,40 @@
 #include "simple_shell.h"
 
 /**
+ * change_status - changes the status of error message struct
+ * @estat: the struct containing the status
+ * @msg: the error messsage
+ * @cd: the code
+ * @flag: should we exit the shell, 1 is yes, 0 is no
+ * Return: Struct.
+ */
+exit_t *change_status(exit_t *estat, char *msg, int cd, int flag)
+{
+	if (msg)
+		estat->message = msg;
+	if (flag == 1 || flag == 0)
+		estat->exit = flag;
+	estat->code = cd;
+	return (estat);
+}
+
+/**
  * check_builtins - checks for built-in commands
  * @token: the string to check if matches known built-ins
- * @inputcpy2: the buffer to be freed if exit is triggered
+ * @cpy2: the buffer to be freed if exit is triggered
  * @arrtok: the array of strings to be freed if exit is triggered
  * Return: an integer to indicate success (0) or failure (1)
  */
-int check_builtins(char *token, char *inputcpy2, char **arrtok)
+int check_builtins(char *token, char *cpy2, char **arrtok)
 {
 	if (get_cmd_func(token))
 	{
 		if (get_cmd_func(token)(""))
 		{
-			_free(3, token, inputcpy2, arrtok);
+			_free(3, token, cpy2, arrtok);
 			return (1);
 		}
-		_free(3, token, inputcpy2, arrtok);
+		_free(3, token, cpy2, arrtok);
 		return (0);
 	}
 
@@ -27,56 +45,56 @@ int check_builtins(char *token, char *inputcpy2, char **arrtok)
  * proc - a function to execute processes and tokenize a string input
  * @input: the input string to be tokenized and executed
  * @ipname: the name of the program being run
+ * @estat: the struct containing exit information
  * Return: an integer to indicate success (1) or failure (0)
  */
-int proc(char *input, char *ipname)
+exit_t *proc(char *input, char *ipname, exit_t *estat)
 {
 	pid_t child_pid;
 	int status, i;
-	char **arrtok, *inputcpy, *inputcpy2;
+	char **arrtok, *inputcpy, *cpy2;
 
-	i = _strlen(input);
-	mem_init(4, &inputcpy, i, &inputcpy2, i);
-	inputcpy = _strcpy(input, inputcpy), inputcpy2 = _strcpy(inputcpy, inputcpy2);
-	i = count_tokens(inputcpy, " ");
-	_free(1, inputcpy), mem_init_two(2, &arrtok, i);
-	arrtok = create_arrtok(inputcpy2, arrtok);
-	arrtok[0] = transform_tok(arrtok[0]);
-	if (check_builtins(arrtok[0], inputcpy2, arrtok))
-		return (1);
+	i = _strlen(input), mem_init(4, &inputcpy, i, &cpy2, i);
+	inputcpy = _strcpy(input, inputcpy), cpy2 = _strcpy(inputcpy, cpy2);
+	i = count_tokens(inputcpy, " "), _free(1, inputcpy);
+	mem_init_two(2, &arrtok, i);
+	arrtok = create_arrtok(cpy2, arrtok), arrtok[0] = transform_tok(arrtok[0]);
+	if (check_builtins(arrtok[0], cpy2, arrtok))
+	{
+		estat = change_status(estat, "Exit", estat->code, 1);
+		return (estat);
+	}
 	child_pid = fork();
 	if (child_pid == -1)
 	{
-		_free(3, arrtok[0], inputcpy2, arrtok);
-		perror("Error:");
-		return (1);
+		_free(3, arrtok[0], cpy2, arrtok), perror("Error:");
+		return (estat);
 	}
 	else if (child_pid == 0)
 	{
 		if (arrtok[0] == NULL || *(arrtok[0]) == '\0')
 		{
-			_free(3, arrtok[0], inputcpy2, arrtok);
-			return (1);
+			_free(3, arrtok[0], cpy2, arrtok);
+			return (estat);
 		}
 		if (execve(arrtok[0], arrtok, environ) == -1)
 		{
-			_free(3, arrtok[0], inputcpy2, arrtok);
-			perror(ipname);
-			return (1);
+			_free(3, arrtok[0], cpy2, arrtok), perror(ipname);
+			estat = change_status(estat, NULL, 127, 1);
+			return (estat);
 		}
 	}
 	else if (child_pid != 0)
 	{
-		wait(&status);
-		_free(3, arrtok[0], inputcpy2, arrtok);
+		wait(&status), _free(3, arrtok[0], cpy2, arrtok);
+		estat = change_status(estat, NULL, WEXITSTATUS(status), estat->exit);
 	}
-	return (0);
+	return (estat);
 }
 
 /**
  * niproc - handles execution of commands in non-interactive mode
  * @av: the commands passed into non-interactive shell
- *
  * Return: An integer to indicate success (1) or failure (0).
  */
 int niproc(char *av[])
